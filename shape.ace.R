69596291999651cf57d6a6b18cd155bbfe45cf1c
Antigoni Kaliontzopoulou <antkal@gmail.com>
2017-09-26 10:08:45 +0100

    Notes and updates 26SEP2017

---
+++ shape.ace.R
@@ -0,0 +1,19 @@
+# Separate function for ace of GM data
+#follows fastAnc in phytools
+
+shape.ace <- function(x, phy){
+  N<-length(phy$tip.label)
+  Nnode <- phy$Nnode
+  anc.states<-NULL   
+  for (i in 1:ncol(x)){
+    x1<-x[,i]
+    tmp <- vector()
+    for (j in 1:Nnode + N) {
+      a <- multi2di(root(phy, node = j))
+      tmp[j - N] <- ace(x1, a, method = "pic")$ace[1]
+    }
+    anc.states<-cbind(anc.states,tmp)   }
+  colnames(anc.states)<-NULL
+  row.names(anc.states)<-1:length(tmp)
+  
+}

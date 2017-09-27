44c084f23fb5eafd77274ed055524c743b72156a
Antigoni Kaliontzopoulou <antkal@gmail.com>
2017-09-27 17:10:45 +0100

    Small update to shape.ace from our discussion

--- shape.ace.R
+++ shape.ace.R
@@ -2,18 +2,19 @@
 #follows fastAnc in phytools
 
 shape.ace <- function(x, phy){
-  N<-length(phy$tip.label)
+  N <- length(phy$tip.label)
   Nnode <- phy$Nnode
   anc.states<-NULL   
   for (i in 1:ncol(x)){
-    x1<-x[,i]
+    x1 <- x[,i]
     tmp <- vector()
     for (j in 1:Nnode + N) {
       a <- multi2di(root(phy, node = j))
       tmp[j - N] <- ace(x1, a, method = "pic")$ace[1]
+      }
+    anc.states<-cbind(anc.states,tmp)   
     }
-    anc.states<-cbind(anc.states,tmp)   }
-  colnames(anc.states)<-NULL
-  row.names(anc.states)<-1:length(tmp)
-  
+  colnames(anc.states) <- NULL
+  row.names(anc.states) <- 1:length(tmp)
+  return(anc.states)
 }

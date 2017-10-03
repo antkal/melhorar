57c8b1c07301ef836a128207e9af19e92579e778
Emma Sherratt <emma.sherratt@gmail.com>
2017-10-03 18:59:16 +1030

    updated

--- testing functions.R
+++ testing functions.R
@@ -2,10 +2,12 @@
 test <- gm.prcomp(Y.gpa$coords)
 plot(test$pc.scores, pch=19, col=rainbow(dim(Y.gpa$coords)[3]))
 
-test.phy <- gm.prcomp(A=Y.gpa$coords, phy=plethspecies$phy)
+test.phy <- gm.prcomp(A=Y.gpa$coords, phy=plethspecies$phy, phylo.pca = TRUE)
 plot(test.phy$pc.scores, pch=19, col=rainbow(dim(Y.gpa$coords)[3]))
 
 library(phytools)
 test.phy2 <- phyl.pca(tree=plethspecies$phy, Y=two.d.array(Y.gpa$coords))
 plot(test.phy2$S, pch=19, col=rainbow(dim(Y.gpa$coords)[3]))
 # not same. What are we doing differently?
+
+
